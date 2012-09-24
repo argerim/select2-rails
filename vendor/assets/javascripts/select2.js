4a13a4b2c6871a77b9e59eafa0151853499ffd43
Pedro Nascimento <pnascimento@gmail.com>
2012-09-24 13:36:21 -0300

    Updating to content upstream.

--- vendor/assets/javascripts/select2.js
+++ vendor/assets/javascripts/select2.js
@@ -13,23 +13,23 @@
  See the License for the specific language governing permissions and limitations under the License.
  */
  (function ($) {
-  if(typeof $.fn.each2 == "undefined"){
-    $.fn.extend({
-      /*
-      * 4-10 times faster .each replacement
-      * use it carefully, as it overrides jQuery context of element on each iteration
-      */
-      each2 : function (c) {
-        var j = $([0]), i = -1, l = this.length;
-        while (
-          ++i < l
-          && (j.context = j[0] = this[i])
-          && c.call(j[0], i, j) !== false //"this"=DOM, i=index, j=jQuery object
-        );
-        return this;
-      }
-    });
-  }
+ 	if(typeof $.fn.each2 == "undefined"){
+ 		$.fn.extend({
+ 			/*
+			* 4-10 times faster .each replacement
+			* use it carefully, as it overrides jQuery context of element on each iteration
+			*/
+			each2 : function (c) {
+				var j = $([0]), i = -1, l = this.length;
+				while (
+					++i < l
+					&& (j.context = j[0] = this[i])
+					&& c.call(j[0], i, j) !== false //"this"=DOM, i=index, j=jQuery object
+				);
+				return this;
+			}
+ 		});
+ 	}
 })(jQuery);
 
 (function ($, undefined) {
@@ -173,7 +173,7 @@
      * the elements under the pointer are scrolled.
      */
     function installFilteredMouseMove(element) {
-      element.bind("mousemove", function (e) {
+	    element.bind("mousemove", function (e) {
             var lastpos = $.data(document, "select2-lastpos");
             if (lastpos === undefined || lastpos.x !== e.pageX || lastpos.y !== e.pageY) {
                 $(e.target).trigger("mousemove-filtered", e);
@@ -230,21 +230,21 @@
 
     function measureTextWidth(e) {
         if (!sizer){
-          var style = e[0].currentStyle || window.getComputedStyle(e[0], null);
-          sizer = $("<div></div>").css({
-              position: "absolute",
-              left: "-10000px",
-              top: "-10000px",
-              display: "none",
-              fontSize: style.fontSize,
-              fontFamily: style.fontFamily,
-              fontStyle: style.fontStyle,
-              fontWeight: style.fontWeight,
-              letterSpacing: style.letterSpacing,
-              textTransform: style.textTransform,
-              whiteSpace: "nowrap"
-          });
-          $("body").append(sizer);
+        	var style = e[0].currentStyle || window.getComputedStyle(e[0], null);
+        	sizer = $("<div></div>").css({
+	            position: "absolute",
+	            left: "-10000px",
+	            top: "-10000px",
+	            display: "none",
+	            fontSize: style.fontSize,
+	            fontFamily: style.fontFamily,
+	            fontStyle: style.fontStyle,
+	            fontWeight: style.fontWeight,
+	            letterSpacing: style.letterSpacing,
+	            textTransform: style.textTransform,
+	            whiteSpace: "nowrap"
+	        });
+        	$("body").append(sizer);
         }
         sizer.text(e.val());
         return sizer.width();
@@ -1114,7 +1114,7 @@
         highlightUnderEvent: function (event) {
             var el = $(event.target).closest(".select2-result-selectable");
             if (el.length > 0 && !el.is(".select2-highlighted")) {
-            var choices = this.results.find('.select2-result-selectable');
+        		var choices = this.results.find('.select2-result-selectable');
                 this.highlight(choices.index(el));
             } else if (el.length == 0) {
                 // if we are over an unselectable item remove al highlights
@@ -1198,8 +1198,8 @@
             if (opts.maximumSelectionSize >=1) {
                 data = this.data();
                 if ($.isArray(data) && data.length >= opts.maximumSelectionSize && checkFormatter(opts.formatSelectionTooBig, "formatSelectionTooBig")) {
-                  render("<li class='select2-selection-limit'>" + opts.formatSelectionTooBig(opts.maximumSelectionSize) + "</li>");
-                  return;
+            	    render("<li class='select2-selection-limit'>" + opts.formatSelectionTooBig(opts.maximumSelectionSize) + "</li>");
+            	    return;
                 }
             }
 
@@ -1373,7 +1373,7 @@
 
         // single
 
-    createContainer: function () {
+		createContainer: function () {
             var container = $("<div></div>", {
                 "class": "select2-container"
             }).html([
@@ -1785,7 +1785,7 @@
                 "   <ul class='select2-results'>" ,
                 "   </ul>" ,
                 "</div>"].join(""));
-      return container;
+			return container;
         },
 
         // multi
@@ -1980,7 +1980,7 @@
             this.parent.opening.apply(this, arguments);
 
             this.clearPlaceholder();
-      this.resizeSearch();
+			this.resizeSearch();
             this.focusSearch();
         },
 
@@ -2165,7 +2165,7 @@
         resizeSearch: function () {
 
             var minimumWidth, left, maxWidth, containerLeft, searchWidth,
-              sideBorderPadding = getSideBorderPadding(this.search);
+            	sideBorderPadding = getSideBorderPadding(this.search);
 
             minimumWidth = measureTextWidth(this.search) + 10;
 

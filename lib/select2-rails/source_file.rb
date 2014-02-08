4aea96b2d5f507e110d6f1f4daf239b130939c72
Andrey Koleshko <ka8725@gmail.com>
2014-02-09 01:36:56 +0300

    Add latest option for fetch thor task

--- lib/select2-rails/source_file.rb
+++ lib/select2-rails/source_file.rb
@@ -6,9 +6,14 @@ class SourceFile < Thor
   include Thor::Actions
 
   desc "fetch source files", "fetch source files from GitHub"
+  option :latest, type: :boolean, default: false, aliases: :l
   def fetch
     filtered_tags = fetch_tags
-    tag = select("Which tag do you want to fetch?", filtered_tags)
+    tag = if options[:latest]
+            filtered_tags.last
+          else
+            select("Which tag do you want to fetch?", filtered_tags)
+          end.value
     self.destination_root = "app/assets"
     remote = "https://github.com/ivaynberg/select2"
     get "#{remote}/raw/#{tag}/select2.png", "images/select2.png"
@@ -35,11 +40,18 @@ class SourceFile < Thor
     self.destination_root = "app/assets"
     remove_file "stylesheets/select2.css"
   end
+
   private
   def fetch_tags
     http = HTTPClient.new
     response = JSON.parse(http.get("https://api.github.com/repos/ivaynberg/select2/tags").body)
-    response.map{|tag| tag["name"]}.sort
+    tags = response.map{|tag| tag["name"]}.sort
+
+    [].tap do |result|
+      tags.each_with_index do |tag, index|
+        result << OpenStruct.new(index: index + 1, value: tag)
+      end
+    end
   end
   def languages
     [ "ar", "bg", "ca", "cs", "da", "de", "el", "es", "et", "eu", "fa", "fi", "fr", "gl", "he", "hr",
@@ -47,11 +59,11 @@ class SourceFile < Thor
       "pt-PT", "ro", "ru", "sk", "sv", "th", "tr", "ua", "vi", "zh-CN", "zh-TW"
     ].sort
   end
-  def select msg, elements
-    elements.each_with_index do |element, index|
-      say(block_given? ? yield(element, index + 1) : ("#{index + 1}. #{element.to_s}"))
+  def select msg, tags
+    tags.each do |tag|
+      say("#{tag.index}. #{tag.value}")
     end
     result = ask(msg).to_i
-    elements[result - 1]
+    tags[result - 1]
   end
 end

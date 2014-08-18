75330b7361cbd5fc0b2299b8d06a7e2341cc8ca6
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-18 19:37:16 +0200

    bugfix asset precompilation

--- lib/select2-rails/engine.rb
+++ lib/select2-rails/engine.rb
@@ -1,9 +1,11 @@
 module Select2
   module Rails
     class Engine < ::Rails::Engine
-      Rails.application.config.assets.paths << root.join('vendor', 'assets', 'images')
-      Rails.application.config.assets.paths << root.join('vendor', 'assets', 'stylesheets')
-      Rails.application.config.assets.paths << root.join('vendor', 'assets', 'javascripts')
+      initializer :assets do |config|
+        Rails.application.config.assets.paths << root.join('vendor', 'assets', 'images')
+        Rails.application.config.assets.paths << root.join('vendor', 'assets', 'stylesheets')
+        Rails.application.config.assets.paths << root.join('vendor', 'assets', 'javascripts')
+      end
     end
   end
 end

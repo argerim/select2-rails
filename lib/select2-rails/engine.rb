79e8afd8415a2b3236ae57c3ed652051ece2be82
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-18 23:04:44 +0200

    remove unneccessary initializer

--- lib/select2-rails/engine.rb
+++ lib/select2-rails/engine.rb
@@ -1,11 +1,6 @@
 module Select2
   module Rails
     class Engine < ::Rails::Engine
-      initializer :assets do |app|
-        app.config.assets.paths << root.join('vendor', 'assets', 'images')
-        app.config.assets.paths << root.join('vendor', 'assets', 'stylesheets')
-        app.config.assets.paths << root.join('vendor', 'assets', 'javascripts')
-      end
     end
   end
 end

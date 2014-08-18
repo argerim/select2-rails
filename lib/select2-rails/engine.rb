f9ee40f4c54691f4d34208be79b5bcb04492f2d7
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-18 19:42:43 +0200

    more bugfixing again

--- lib/select2-rails/engine.rb
+++ lib/select2-rails/engine.rb
@@ -1,10 +1,10 @@
 module Select2
   module Rails
     class Engine < ::Rails::Engine
-      initializer :assets do |config|
-        config.assets.paths << root.join('vendor', 'assets', 'images')
-        config.assets.paths << root.join('vendor', 'assets', 'stylesheets')
-        config.assets.paths << root.join('vendor', 'assets', 'javascripts')
+      initializer :assets do |app|
+        app.config.assets.paths << root.join('vendor', 'assets', 'images')
+        app.config.assets.paths << root.join('vendor', 'assets', 'stylesheets')
+        app.config.assets.paths << root.join('vendor', 'assets', 'javascripts')
       end
     end
   end

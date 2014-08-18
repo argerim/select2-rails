0d950a899e7dde6ce6e15f3decfa61c65cec748f
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-18 23:15:52 +0200

    precompile images

--- lib/select2-rails/engine.rb
+++ lib/select2-rails/engine.rb
@@ -1,6 +1,10 @@
 module Select2
   module Rails
     class Engine < ::Rails::Engine
+      initializer :images do |app|
+        app.config.assets.precompile +=
+          %w(select2.png select2-spinner.gif select2x2.png)
+      end
     end
   end
 end

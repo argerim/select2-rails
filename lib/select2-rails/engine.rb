4bc678178d766aab414fe52e11ed52f6ab12fdcd
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-18 19:35:14 +0200

    update engine assets paths

--- lib/select2-rails/engine.rb
+++ lib/select2-rails/engine.rb
@@ -1,6 +1,9 @@
 module Select2
   module Rails
     class Engine < ::Rails::Engine
+      Rails.application.config.assets.paths << root.join('vendor', 'assets', 'images')
+      Rails.application.config.assets.paths << root.join('vendor', 'assets', 'stylesheets')
+      Rails.application.config.assets.paths << root.join('vendor', 'assets', 'javascripts')
     end
   end
 end

ab931b81753169dea639c85993b2808ae3e7b791
dota? =op <rogi@skylittlesystem.org>
2016-10-02 18:13:59 -0300

    Add assets paths (fix #164).
    Looks like a similar change was done and undone several tiems in teh
    past...

--- lib/select2-rails/engine.rb
+++ lib/select2-rails/engine.rb
@@ -1,6 +1,12 @@
 module Select2
   module Rails
     class Engine < ::Rails::Engine
+      # NOTE: this is necessary for rails 5
+      initializer 'select2-rails.assets.precompile' do |app|
+        %w(stylesheets javascripts).each do |sub|
+          app.config.assets.paths << root.join('vendor', 'assets', sub).to_s
+        end
+      end
     end
   end
 end

6ce878e8d8d231234cf4f24b954b28cb837e89cb
fedeagripa <fagripa@toptierlabs.com>
2017-09-07 17:55:36 -0300

    Add padding because cross is overlapping options and the click is hitting both... yes, is doing that

--- vendor/assets/javascripts/select2-full.js
+++ vendor/assets/javascripts/select2-full.js
@@ -4267,7 +4267,8 @@ S2.define('select2/dropdown/attachBody',[
 
     var css = {
       left: offset.left,
-      top: container.bottom
+      top: container.bottom,
+      padding: '0 0 20px 0'
     };
 
     // Determine what the parent element is to use for calciulating the offset
